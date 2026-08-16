2354258afc995fb9a81e4a8b9cd10894445b3725
lmz00701 <qflmz2007@163.com>
2026-08-17 01:15:34 +0800

    Update notes-data.js

--- study/notes-data.js
+++ study/notes-data.js
@@ -20,22 +20,22 @@
 const noteData = [
 
     // ---- 生物信息 (info) ----
-    { title: 'git教程', url: 'https://bio2025.site/study/info/生物信息学git基础教程.html', tag: 'git教程', category: 'info' },
-    { title: 'R_ggplot2教程', url: 'https://bio2025.site/study/info/ggplot2_tutorial', tag: 'ggplot2教程', category: 'info' },   // ← 示例占位，可删除
-    { title: 'Biopython教程', url: 'https://bio2025.site/study/info/Biopython零基础实战教程.html', tag: 'Biopython零基础实战教程', category: 'info' },
-    { title: '生信基础知识教程', url: 'https://bio2025.site/study/info/生信基础知识教程.html', tag: '配套biopython', category: 'info' },
-    { title: '生信入门教程', url: 'https://bio2025.site/study/info/生信入门教程.html', tag: '生信基础', category: 'info' },
+    { title: 'git教程', url: '/study/info/生物信息学git基础教程.html', tag: 'git教程', category: 'info' },
+    { title: 'R_ggplot2教程', url: '/study/info/ggplot2_tutorial', tag: 'ggplot2教程', category: 'info' },   // ← 示例占位，可删除
+    { title: 'Biopython教程', url: '/study/info/Biopython零基础实战教程.html', tag: 'Biopython零基础实战教程', category: 'info' },
+    { title: '生信基础知识教程', url: '/study/info/生信基础知识教程.html', tag: '配套biopython', category: 'info' },
+    { title: '生信入门教程', url: 'e/study/info/生信入门教程.html', tag: '生信基础', category: 'info' },
 
 
     // ---- 英语 (en) ----
-    { title: '26.12四级作文预测范文', url: 'https://bio2025.site/study/en/26.12四级作文', tag: '四级作文', category: 'en' },
-    { title: '四级作文', url: 'https://bio2025.site/study/en/cet4-1', tag: '四级作文', category: 'en' },
+    { title: '26.12四级作文预测范文', url: '/study/en/26.12四级作文', tag: '四级作文', category: 'en' },
+    { title: '四级作文', url: '/study/en/cet4-1', tag: '四级作文', category: 'en' },
 
     // ---- 生物学 (bio) ----
     { title: 'example', url: '#', tag: 'example', category: 'bio' },    // ← 示例占位，可删除
 
     // ---- 数学 (math) ----
-    { title: '8月数竞规划', url: 'https://bio2025.site/study/math/八月数竞', tag: '复习重点', category: 'math' },
+    { title: '8月数竞规划', url: '/study/math/八月数竞', tag: '复习重点', category: 'math' },
 
 ];
 
